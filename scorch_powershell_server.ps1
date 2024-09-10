bdcc3982d4cffd645b9db2a2c1788782f5a68c9c
Zeus <64803358+zeusofyork@users.noreply.github.com>
2024-09-10 12:38:57 -0400

    Update scorch_powershell_server.ps1

--- scorch_powershell_server.ps1
+++ scorch_powershell_server.ps1
@@ -1,1 +1,1 @@
-Start-Process -FilePath "powershell.exe" -ArgumentList "Invoke-WebRequest -Uri 'http://localhost:8080' -OutFile 'SCORCH_dashboard.html'"
+Start-Process -FilePath "powershell.exe" -ArgumentList "Invoke-WebRequest -Uri 'http://localhost:8080' -OutFile 'dashboard.html'"
